f80a13a04ba17616cb55b5fdaca84d4520fbf6bc
Erez Binyamin <ezbn2532@gmail.com>
2022-05-28 10:41:18 -0400

    getData.sh: clean up

--- app/data/getData.sh
+++ app/data/getData.sh
@@ -4,7 +4,8 @@ pushd .
 rm -rf raw; mkdir raw
 cd raw
 wget 'https://www.federalreserve.gov/releases/z1/dataviz/download/zips/z1-visualization.zip'
-unzip z1-visualization.zip
+unzip z1-visualization.zip; rm z1-visualization.zip
 wget 'https://www.federalreserve.gov/econres/scf/dataviz/download/zips/scf.zip'
-unzip scf.zip
+unzip scf.zip; rm scf.zip
+rm -f *.txt z1-visualization-data.csv
 popd
