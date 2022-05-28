a06e45e3df315d079403eb136afd1edbdc32a2b5
Erez Binyamin <ezbn2532@gmail.com>
2022-05-28 11:41:40 -0400

    getData.sh: swapped wget with curl

--- app/data/getData.sh
+++ app/data/getData.sh
@@ -3,9 +3,9 @@ rm *.zip *.csv *.txt
 pushd .
 rm -rf raw; mkdir raw
 cd raw
-wget 'https://www.federalreserve.gov/releases/z1/dataviz/download/zips/z1-visualization.zip'
-unzip z1-visualization.zip; rm z1-visualization.zip
-wget 'https://www.federalreserve.gov/econres/scf/dataviz/download/zips/scf.zip'
-unzip scf.zip; rm scf.zip
+curl -ko 'z1-visualization.zip' 'https://www.federalreserve.gov/releases/z1/dataviz/download/zips/z1-visualization.zip'
+unzip 'z1-visualization.zip'; rm 'z1-visualization.zip'
+curl -ko 'scf.zip' 'https://www.federalreserve.gov/econres/scf/dataviz/download/zips/scf.zip'
+unzip 'scf.zip'; rm 'scf.zip'
 rm -f *.txt z1-visualization-data.csv *_have.*
 popd
