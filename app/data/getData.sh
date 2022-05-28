b5da920762b8f33ab9e22c73c0424a61b85603f7
Erez Binyamin <ezbn2532@gmail.com>
2022-05-28 12:38:53 -0400

    getData.sh: Added international GDP

--- app/data/getData.sh
+++ app/data/getData.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-rm *.zip *.csv *.txt
 pushd .
-rm -rf raw; mkdir raw
+
+rm -rf raw
+mkdir raw
 cd raw
+
 curl -ko 'z1-visualization.zip' 'https://www.federalreserve.gov/releases/z1/dataviz/download/zips/z1-visualization.zip'
-unzip 'z1-visualization.zip'; rm 'z1-visualization.zip'
+unzip 'z1-visualization.zip'
+rm 'z1-visualization.zip' z1-visualization-data.csv *.txt
+
 curl -ko 'scf.zip' 'https://www.federalreserve.gov/econres/scf/dataviz/download/zips/scf.zip'
-unzip 'scf.zip'; rm 'scf.zip'
-rm -f *.txt z1-visualization-data.csv *_have.*
+unzip 'scf.zip'
+rm -f 'scf.zip' *_have.*
+
+curl -ko 'world_gdp.zip' 'https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=csv'
+unzip 'world_gdp.zip'
+rm -f 'world_gdp.zip'
+
 popd
