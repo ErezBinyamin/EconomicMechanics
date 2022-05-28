d06c07a05de7d25d6a5e51451225288538ba8353
Erez Binyamin <ezbn2532@gmail.com>
2022-05-28 10:39:05 -0400

    getData.sh: pull data into raw

--- app/data/getData.sh
+++ app/data/getData.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-pushd .
-cd /tmp
 rm *.zip *.csv *.txt
-
+pushd .
+rm -rf raw; mkdir raw
+cd raw
 wget 'https://www.federalreserve.gov/releases/z1/dataviz/download/zips/z1-visualization.zip'
-unzip /tmp/z1-visualization.zip
-
+unzip z1-visualization.zip
 wget 'https://www.federalreserve.gov/econres/scf/dataviz/download/zips/scf.zip'
-unzip /tmp/scf.zip
-
+unzip scf.zip
 popd
